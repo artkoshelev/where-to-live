099c97030158cfc1da1806fa3e1361a77a80282c
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2013-09-24 14:21:51 +0600

    [android] some misc design changes

--- client/wtl-android/wtl-android/src/main/java/ru/yandex/hackaton/wheretolive/ui/activity/MainActivity.java
+++ client/wtl-android/wtl-android/src/main/java/ru/yandex/hackaton/wheretolive/ui/activity/MainActivity.java
@@ -2,6 +2,9 @@ package ru.yandex.hackaton.wheretolive.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -23,11 +26,11 @@ import ru.yandex.hackaton.wheretolive.utils.DeviceUtil;
  * Created with IntelliJ IDEA. User: rustamgaifullin Date: 9/14/13 Time: 6:07 PM To change this
  * template use File | Settings | File Templates.
  */
-public class WizardActivity extends Activity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.wizard_activity);
+        setContentView(R.layout.main_activity);
 
         DeviceUtil.init(this);
         try {
@@ -35,32 +38,52 @@ public class WizardActivity extends Activity {
                     .addResponseListener(new OnResponseListener<DistrictResponse>() {
                         @Override
                         public void onOk(DistrictResponse param) {
-                            WtlUtilsImpl wtlUtils = WtlUtils.Factory.get(WizardActivity.this);
+                            WtlUtilsImpl wtlUtils = WtlUtils.Factory.get(MainActivity.this);
                             wtlUtils.insertDistrict(param.getDistrict());
                         }
 
                         @Override
                         public void onError(Throwable throwable) {
-                            Toast.makeText(WizardActivity.this, "error", Toast.LENGTH_LONG).show();
+                            Toast.makeText(MainActivity.this, "error", Toast.LENGTH_LONG).show();
                         }
                     }));
             WtlClient.request(new CategoryHandler(new BaseRequest())
                     .addResponseListener(new OnResponseListener<CategoryResponse>() {
                         @Override
                         public void onOk(CategoryResponse param) {
-                            WtlUtilsImpl wtlUtils = WtlUtils.Factory.get(WizardActivity.this);
+                            WtlUtilsImpl wtlUtils = WtlUtils.Factory.get(MainActivity.this);
                             wtlUtils.insertCategory(param.getmCategory());
                         }
 
                         @Override
                         public void onError(Throwable throwable) {
-                            Toast.makeText(WizardActivity.this, "error", Toast.LENGTH_LONG).show();
+                            Toast.makeText(MainActivity.this, "error", Toast.LENGTH_LONG).show();
                         }
                     }));
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_about:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
     public void onProfileClick(View v){
         ProfileActivity.show(this);
